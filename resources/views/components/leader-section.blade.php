c321da822f1b97dc0463de32eaa60bcfecbe88aa
Muhammad Sufyan <muhammadsufyan112011@gmail.com>
2026-09-11 09:53:40 -0700

    Second section home transperence setting and spacing issue resiolved

--- resources/views/components/leader-section.blade.php
+++ resources/views/components/leader-section.blade.php
@@ -6,7 +6,7 @@
 
 <section class="home-stack-panel leader-section relative z-20" data-home-stack-panel data-leader-section aria-label="Sobre a Depiderme">
     <div
-        class="leader-panel w-full rounded-tl-[40px] bg-white {{ $leaderPadding }} pb-12 pt-10 sm:pb-16 sm:pt-14 md:py-16 lg:rounded-tl-[80px] lg:pb-24 lg:pt-20"
+        class="leader-panel w-full rounded-tl-[40px] bg-white {{ $leaderPadding }} py-12 sm:py-16 md:py-16 lg:rounded-tl-[80px] lg:pb-24 lg:pt-20"
         data-leader-panel
     >
         <h2 class="leader-headline" data-leader-headline>
